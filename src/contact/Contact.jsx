70514b96765211773ab5011d35280eb40502d9e2
Viktor Van Puyvelde <viktorvp2002@gmail.com>
2022-02-18 14:09:39 +0100

    contact form (todo: content)

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import Phone from "../img/phone.png";
 import Email from "../img/email.png";
 
 const Contact = () => {
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="c">
       <div className="c-bg"></div>
@@ -22,12 +28,19 @@ const Contact = () => {
           </div>
         </div>
         <div className="c-right">
-          <div className="c-desc">
+          <p className="c-desc">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Exercitationem aliquam officia maiores perferendis ex tempore alias
             reiciendis pariatur atque necessitatibus, adipisci possimus unde.
             Totam earum quia exercitationem cumque maxime iste!
-          </div>
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Naam" name="user_name" />
+            <input type="text" placeholder="Onderwerp" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Bericht" name="message" />
+            <button>Submit</button>
+          </form>
         </div>
       </div>
     </div>
